0731c4a0624c51b4d116179ab651a94199cfa817
Ragini Negi <104869523+ragininegi@users.noreply.github.com>
2022-07-19 20:55:49 -0700

    Added python file which has Application questions

---
+++ app.py
@@ -0,0 +1,54 @@
+"""
+Welcome to Algo Trader!!
+
+Would you like to download fresh data or use an existing data?
+1 Download Fresh Data
+2 Use existing data
+
+Download Fresh Data 
+    Enter Ticker symbol?
+    User will enter the company Ticker Symbol
+    
+Use existing data
+    Get list of Tickers we want to display
+    Which ticker do you want to use? (Select from the list)
+
+Loading data as per the selected ticker
+
+What type of Algorithm Strategies you want to trade?
+    Donchian's Four Weel Rule
+    Dreyfus’s 52 Week Rule
+    Turtle Trading
+    Golden Cross” 20v200 SMA
+
+
+How many cash do you want to trade?
+Default 100k
+
+How many shares do you want to trade?
+Default 1
+
+Run Strategies
+
+Want to plot the results?
+Yes
+No
+
+If yes
+Plot Data
+
+If No
+Abort
+
+
+Do you still want to continue
+Yes
+No
+
+If yes
+Main Menu
+Choose different trading strategy
+
+
+
+"""
