771b696326038eb3fde2c25a17c4681a6be2d935
Peter Paquette <git@peterpaquette.com>
2022-07-21 04:40:54 -0700

    Use this to quickly plot graphs

--- work-in-progress/test_code.py
+++ work-in-progress/test_code.py
@@ -1,30 +1,28 @@
 import sys
 import os
-  
-# getting the name of the directory
-# where the this file is present.
-current = os.path.dirname(os.path.realpath(__file__))
-  
-# Getting the parent directory name
-# where the current directory is present.
-parent = os.path.dirname(current)
-  
-# adding the parent directory to 
-# the sys.path.
-sys.path.append(parent)
-  
-# now we can import the module in the parent
-# directory.
+
+IN_ROOT_DIR = True
+
+if not IN_ROOT_DIR:
+	# getting the name of the directory
+	# where the this file is present.
+	current = os.path.dirname(os.path.realpath(__file__))
+	
+	# Getting the parent directory name
+	# where the current directory is present.
+	parent = os.path.dirname(current)
+	
+	# adding the parent directory to 
+	# the sys.path.
+	sys.path.append(parent)
+	
+	# now we can import the module in the parent directory.
 
 from utils import *
+from plots import *
 
-data = download_stock_data()
+stock_plot_df = load_data('plot_stock_data')
+trade_plot_df = load_data('plot_trade_data')
+broke_plot_df = load_data('plot_broke_data')
 
-for i in data:
-	data[i].reset_index(inplace=True)
-
-print(data['SPY'])
-
-slice = data['SPY'][['Date','Close','Volume']]
-
-print(slice)
+generate_graph(stock_plot_df, trade_plot_df, broke_plot_df)
